8aa61f7a4384553a27612b73fe8b1a123895e918
Dominik Spitzli <dominik@spitzli.dev>
2022-06-14 20:37:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,29 +2,29 @@
 
 PS3='Please enter your choice: '
 
-         ls -la /usr/bin/
+ls -la /usr/bin/
 options=("Discord.js" "Discord.py")
 
 select opt in "${options[@]}"
 do
     case $opt in
         "Discord.js")
-         echo ls -la /usr/bin/
+            echo ls -la /usr/bin/
             apt install nodejs npm;
             ls -la /usr/bin/
             /usr/local/bin/npm install discord.js;
             /usr/local/bin/node ${START_FILE}
             break
-            ;;
+        ;;
         "Discord.py")
             apt install python3-pip;
             pip3 install discord;
             python3 ${START_FILE}
             break
-            ;;
-            "stop")
+        ;;
+        "stop")
             exit 0
-            ;;
+        ;;
         *) echo "invalid option $REPLY";;
     esac
 done
